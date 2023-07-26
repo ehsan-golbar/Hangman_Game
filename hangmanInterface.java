dedf5b081f1fe13db628c644458b5de7ccefbc6a
Ehsan <1382ehg@gmail.com>
2023-07-26 17:41:52 +0330

    defining Hangman Interface with three main methodes

---
+++ hangmanInterface.java
@@ -0,0 +1,38 @@
+interface Hangman {
+    public String selected_word ( boolean easy, boolean medium, boolean hard ) ; //select the word thaat user should guess  : Reza
+    public boolean user_guess (String selected_word) ;
+        
+        // take character from user
+        // compare with selected word and showing
+        // delete the selected character
+        // moving peson and shark
+
+    
+
+    public void new_game () ; // Ehsan
+        // asking the level of game from user
+        // start loop
+            // call selected_word and user_guess
+            // show win or lose 
+        // save the score and return 
+
+
+}
+
+
+class shark {//singleton
+
+}
+
+
+class person {//singleton
+
+}
+
+
+
+easy 
+medium
+hard
+costum
+hardcore
