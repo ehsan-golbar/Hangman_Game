4c7ae08e270f17d6a245b9fdabe240477826d0f8
Ehsan <1382ehg@gmail.com>
2023-07-30 19:06:17 +0330

    devide the game to three section as follows : registration, choose_level, start_game

--- entry_page.py
+++ entry_page.py
@@ -1,8 +1,18 @@
 from tkinter import *
 # def set_level():
 #     #test
-#     lable = Label(entry_page, text=level_choice.get())
+#     lable = Label(level_frame, text=level_choice.get())
 #     lable.pack()
+def choose_level():
+    level_frame.pack()
+    registration_frame.pack_forget()
+
+
+def start_game():
+    game_frame.pack()
+    level_frame.pack_forget()
+    Label(game_frame, text="salam").pack()
+
 
 entry_page = Tk()
 entry_page.title("HangMan")
@@ -18,20 +28,37 @@ entry_posy = screen_height/2 - entry_page_height/2
 
 entry_page.geometry("%dx%d+%d+%d" % (entry_page_weight, entry_page_height, entry_posx, entry_posy))
 
-# welcome lable
-welcome_lable = Label(entry_page, text="Welcome to HangMan Game")
+# registreation section
+registration_frame = Frame(entry_page)
+registration_frame.pack()
+welcome_lable = Label(registration_frame, text="Welcome to HangMan Game")
 welcome_lable.pack()
+
+
+
+
+
+pass_registration_section_button = Button(registration_frame, text="Next", command=choose_level)
+
+pass_registration_section_button.pack()
 
 # choosing level
 level_frame = Frame(entry_page)
-level_frame.pack()
+
 level_choice = StringVar()
 level_choice.set("easy")
-Radiobutton(level_frame, text="Easy", variable=level_choice, value="easy", command=set_level).pack()
-Radiobutton(level_frame, text="Medium", variable=level_choice, value="medium", command=set_level).pack()
-Radiobutton(level_frame, text="Hard", variable=level_choice, value="hard", command=set_level).pack()
-Radiobutton(level_frame, text="Hardcore", variable=level_choice, value="hardcore", command=set_level).pack()
-Radiobutton(level_frame, text="Costume", variable=level_choice, value="costume", command=set_level).pack()
+Radiobutton(level_frame, text="Easy", variable=level_choice, value="easy").pack()
+Radiobutton(level_frame, text="Medium", variable=level_choice, value="medium").pack()
+Radiobutton(level_frame, text="Hard", variable=level_choice, value="hard").pack()
+Radiobutton(level_frame, text="Hardcore", variable=level_choice, value="hardcore").pack()
+Radiobutton(level_frame, text="Costume", variable=level_choice, value="costume").pack()
+pass_level_section_button = Button(level_frame, text= "Next", command= start_game)
+pass_level_section_button.pack()
+
+# start game section
+game_frame = Frame(entry_page)
+
+
 
 
 
