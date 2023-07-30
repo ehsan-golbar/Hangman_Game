27d6ea7a6d1ad944a2a3c4040183cd91d049d864
Ehsan <1382ehg@gmail.com>
2023-07-30 18:30:34 +0330

    ceating radio button for choosing level

--- entry_page.py
+++ entry_page.py
@@ -1,4 +1,9 @@
 from tkinter import *
+# def set_level():
+#     #test
+#     lable = Label(entry_page, text=level_choice.get())
+#     lable.pack()
+
 entry_page = Tk()
 entry_page.title("HangMan")
 # entry page size and position
@@ -12,6 +17,21 @@ entry_posx = screen_weight/2 - entry_page_weight/2
 entry_posy = screen_height/2 - entry_page_height/2
 
 entry_page.geometry("%dx%d+%d+%d" % (entry_page_weight, entry_page_height, entry_posx, entry_posy))
+
+# welcome lable
+welcome_lable = Label(entry_page, text="Welcome to HangMan Game")
+welcome_lable.pack()
+
+# choosing level
+level_frame = Frame(entry_page)
+level_frame.pack()
+level_choice = StringVar()
+level_choice.set("easy")
+Radiobutton(level_frame, text="Easy", variable=level_choice, value="easy", command=set_level).pack()
+Radiobutton(level_frame, text="Medium", variable=level_choice, value="medium", command=set_level).pack()
+Radiobutton(level_frame, text="Hard", variable=level_choice, value="hard", command=set_level).pack()
+Radiobutton(level_frame, text="Hardcore", variable=level_choice, value="hardcore", command=set_level).pack()
+Radiobutton(level_frame, text="Costume", variable=level_choice, value="costume", command=set_level).pack()
 
 
 
