a558ddaf4bf6d3c53b6805e127ea42f75df53fbc
Ehsan <1382ehg@gmail.com>
2023-07-30 16:59:44 +0330

    set size and position of entry page

--- entry_page.py
+++ entry_page.py
@@ -1,2 +1,21 @@
-import tkinter as tk
+from tkinter import *
+entry_page = Tk()
+entry_page.title("HangMan")
+# entry page size and position
+entry_page_height = 500
+entry_page_weight = 500
+
+screen_height  = entry_page.winfo_screenheight()
+screen_weight =  entry_page.winfo_screenwidth()
+
+entry_posx = screen_weight/2 - entry_page_weight/2
+entry_posy = screen_height/2 - entry_page_height/2
+
+entry_page.geometry("%dx%d+%d+%d" % (entry_page_weight, entry_page_height, entry_posx, entry_posy))
+
+
+
+
+
+entry_page.mainloop()
 
