b5970db3eaf9cd68521dbe57d9281ab28f077453
Ehsan <1382ehg@gmail.com>
2023-08-02 13:57:19 +0330

    make code cleaner

--- entry_page.py
+++ entry_page.py
@@ -2,10 +2,8 @@ import tkinter.messagebox
 from tkinter import *
 from selected_word import selected_word
 from user_guess import user_guess
-# def set_level():
-#     #test
-#     lable = Label(level_frame, text=level_choice.get())
-#     lable.pack()
+
+
 def choose_level():
     level_frame.pack()
     registration_frame.pack_forget()
@@ -23,7 +21,6 @@ def start_game():
     game_frame.pack()
     level_frame.pack_forget()
     Label(game_frame, text="salam").pack()
-    print(level_choice.get())
     easy, medium, hard = (False, False, False)
     match level_choice:
         case "easy":
@@ -52,6 +49,7 @@ def show_win_lose(user_won):
     else:
         tkinter.messagebox.showinfo(title="", message="sorry, you lost")
 
+
 def continue_game():
     return tkinter.messagebox.askquestion("continue", "Do you want to continue the game ?")
 
@@ -75,33 +73,16 @@ registration_frame = Frame(entry_page)
 registration_frame.pack()
 welcome_lable = Label(registration_frame, text="Welcome to HangMan Game")
 welcome_lable.pack()
-
-
-
-
-
 pass_registration_section_button = Button(registration_frame, text="Next", command=choose_level)
-
 pass_registration_section_button.pack()
 
 # choosing level
 level_frame = Frame(entry_page)
-
 level_choice = StringVar()
 level_choice.set("easy")
-# Radiobutton(level_frame, text="Easy", variable=level_choice, value="easy").pack()
-# Radiobutton(level_frame, text="Medium", variable=level_choice, value="medium").pack()
-# Radiobutton(level_frame, text="Hard", variable=level_choice, value="hard").pack()
-# Radiobutton(level_frame, text="Hardcore", variable=level_choice, value="hardcore").pack()
-# Radiobutton(level_frame, text="Costume", variable=level_choice, value="costume").pack()
-# pass_level_section_button = Button(level_frame, text= "Next", command= start_game)
-# pass_level_section_button.pack()
 
 # start game section
 game_frame = Frame(entry_page)
-
-
-
 
 entry_page.mainloop()
 
