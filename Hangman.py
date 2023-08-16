eb42316b5a1067cb0b6d44ce3682f94e6c553c49
Ehsan <1382ehg@gmail.com>
2023-08-16 16:42:42 +0330

    add a simple registration section

--- Hangman.py
+++ Hangman.py
@@ -5,7 +5,7 @@ class Hangman(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Hangman")
-
+        # put the window in center of screen
         window_height = 500
         window_width = 500
         screen_height = self.winfo_screenheight()
@@ -13,6 +13,17 @@ class Hangman(tk.Tk):
         window_posx = screen_width / 2 - window_width / 2
         window_posy = screen_height / 2 - window_height / 2
         self.geometry("%dx%d+%d+%d" % (window_width, window_height, window_posx, window_posy))
+        # registration section
+        registration_frame = tk.Frame(self)
+        registration_frame.pack()
+        welcom_lable = tk.Label(registration_frame, text="Welcome!")
+        welcom_lable.pack()
+        go_to_level_section = tk.Button(registration_frame, text="Next")
+        go_to_level_section.pack()
+
+
+
+
         self.mainloop()
 
 
