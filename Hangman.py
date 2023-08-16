3669281d59935a6cba2720f5c2e5702b1da06599
Ehsan <1382ehg@gmail.com>
2023-08-16 16:32:46 +0330

    set the position of window in screen's center

--- Hangman.py
+++ Hangman.py
@@ -1,9 +1,20 @@
 import tkinter as tk
+
+
 class Hangman(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Hangman")
+
+        window_height = 500
+        window_width = 500
+        screen_height = self.winfo_screenheight()
+        screen_width = self.winfo_screenwidth()
+        window_posx = screen_width / 2 - window_width / 2
+        window_posy = screen_height / 2 - window_height / 2
+        self.geometry("%dx%d+%d+%d" % (window_width, window_height, window_posx, window_posy))
         self.mainloop()
+
 
 
 
