c8e7bb4a680175c5d32f6ac2bb164c845046dc9d
Ehsan <1382ehg@gmail.com>
2023-08-16 17:02:54 +0330

    add level section radio buttons

--- Hangman.py
+++ Hangman.py
@@ -14,21 +14,33 @@ class Hangman(tk.Tk):
         window_posy = screen_height / 2 - window_height / 2
         self.geometry("%dx%d+%d+%d" % (window_width, window_height, window_posx, window_posy))
         # registration section
-        registration_frame = tk.Frame(self)
-        registration_frame.pack()
-        welcom_lable = tk.Label(registration_frame, text="Welcome!")
+        self.registration_frame = tk.Frame(self)
+        self.registration_frame.pack()
+        welcom_lable = tk.Label(self.registration_frame, text="Welcome!")
         welcom_lable.pack()
-        go_to_level_section = tk.Button(registration_frame, text="Next")
+        go_to_level_section = tk.Button(self.registration_frame, text="Next", command= self.choose_level)
         go_to_level_section.pack()
-
+        # level section
+        self.level_frame = tk.Frame(self)
+        self.selected_level = tk.StringVar()
+        self.selected_level.set("easy")
+        tk.Radiobutton(self.level_frame, text="Easy", variable= self.selected_level, value="easy").pack()
+        tk.Radiobutton(self.level_frame, text="Medium", variable=self.selected_level, value="medium").pack()
+        tk.Radiobutton(self.level_frame, text="Hard", variable= self.selected_level, value="hard").pack()
+        tk.Radiobutton(self.level_frame, text="Hardcore", variable= self.selected_level, value="hardcore").pack()
+        tk.Radiobutton(self.level_frame, text="Costume", variable= self.selected_level, value="costume").pack()
 
 
 
         self.mainloop()
 
 
+    def choose_level(self):
+        self.level_frame.pack()
+        self.registration_frame.pack_forget()
 
 
-
+    def print_test(self):
+        print(self.selected_level.get())
 
 test = Hangman()
